faa92b152215ce028987f9e3b6f4eb12eb68b648
Henri Hubert <henri.hubert@gitguardian.com>
2022-07-26 18:31:12 +0200

    fix(scripts): make ggcanary_call fail on unexisting user

--- scripts/ggcanary_call.sh
+++ scripts/ggcanary_call.sh
@@ -9,5 +9,11 @@ else
     user_data=$(terraform state pull | jq ".outputs.ggcanary_access_keys.value[] | select(.name==\"$1\")")
     key_id=$(echo $user_data | jq -r .access_key_id)
     key_secret=$(echo $user_data | jq -r .access_key_secret)
-    AWS_ACCESS_KEY_ID=$key_id AWS_SECRET_ACCESS_KEY=$key_secret AWS_DEFAULT_REGION=us-west-2 aws s3 ls
+    if [ "$user_data" = "" ]
+    then
+        echo -e "Error: User $1 not found"
+        exit 1
+    else
+        AWS_ACCESS_KEY_ID=$key_id AWS_SECRET_ACCESS_KEY=$key_secret AWS_DEFAULT_REGION=us-west-2 aws s3 ls
+    fi
 fi
