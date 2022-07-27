f34cb1cc8817c6f81df93aa3706b220dfe3238d9
Henri Hubert <henri.hubert@gitguardian.com>
2022-07-27 13:12:57 +0200

    chore(scripts): make script more resilient
    - Check if user exists in display_ggcanary_credentials
    - Do no print error message with correct operations when making ggcanary_call

--- scripts/ggcanary_call.sh
+++ scripts/ggcanary_call.sh
@@ -7,13 +7,22 @@ else
     echo -e "\nPerforming ggcanary call for USER: $1"
     set -e
     user_data=$(terraform state pull | jq ".outputs.ggcanary_access_keys.value[] | select(.name==\"$1\")")
-    key_id=$(echo $user_data | jq -r .access_key_id)
-    key_secret=$(echo $user_data | jq -r .access_key_secret)
     if [ "$user_data" = "" ]
     then
         echo -e "Error: User $1 not found"
         exit 1
     else
-        AWS_ACCESS_KEY_ID=$key_id AWS_SECRET_ACCESS_KEY=$key_secret AWS_DEFAULT_REGION=us-west-2 aws s3 ls
+        set +e
+        key_id=$(echo $user_data | jq -r .access_key_id)
+        key_secret=$(echo $user_data | jq -r .access_key_secret)
+        aws_response=$(AWS_ACCESS_KEY_ID=$key_id AWS_SECRET_ACCESS_KEY=$key_secret AWS_DEFAULT_REGION=us-west-2 aws s3 ls 2>&1)
+        # expect "An error occurred (AccessDenied) when calling the ListBuckets operation: Access Denied"
+        if [[ "$aws_response" == *AccessDenied* ]]
+        then
+            echo -e "Call performed at $(date)"
+        else
+            echo -e "Unexpected response : $aws_response"
+            exit 1
+        fi
     fi
 fi
